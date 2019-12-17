7f5ef7cfe12704d77e799d78256096b13415fd39
Spyros Ligouras <ligouras@admin.grnet.gr>
2019-12-17 16:49:49 +0200

    Add new user setup script

---
+++ add-user.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -f /var/cache/apt/pkgcache.bin ]; then
+    if (( $(date '+%s') - $(stat -c '%Y' /var/cache/apt/pkgcache.bin) > 86400 )) ; then
+	apt-get update
+    fi
+else
+    apt-get update
+fi
+
+hash curl 2>/dev/null || sudo DEBIAN_FRONTEND=noninteractive apt-get install curl -qq
+hash jq 2>/dev/null || sudo DEBIAN_FRONTEND=noninteractive apt-get install jq -qq
+
+USER=ligouras
+
+getent passwd $USER >/dev/null || useradd -D $USER
+
+if [ "$(id -un)" != "$USER" ]; then
+	sudo -Hu $USER $0 $@
+else
+	install -d -m 0700 $HOME/.ssh
+	install -m 0600 $HOME/.ssh/authorized_keys
+
+	curl https://api.github.com/users/$USER/keys | jq -r .[].key >> $HOME/.ssh/authorized_keys
+fi
