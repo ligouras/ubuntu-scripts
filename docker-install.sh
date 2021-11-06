d8ef0a64daea018798023d22fb9107c545449cd4
Spyros Ligouras <ligouras@admin.grnet.gr>
2021-11-06 10:44:00 +0200

    Update docker-compose version

--- docker-install.sh
+++ docker-install.sh
@@ -47,7 +47,7 @@ fi
 
 # docker-compose
 
-DOCKER_COMPOSE_URL="https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)"
+DOCKER_COMPOSE_URL="https://github.com/docker/compose/releases/download/1.28.4/docker-compose-$(uname -s)-$(uname -m)"
 
 [ -f /usr/local/bin/docker-compose ] || curl -qL ${DOCKER_COMPOSE_URL} -o /usr/local/bin/docker-compose
 [ -x /usr/local/bin/docker-compose ] || chmod +x /usr/local/bin/docker-compose
